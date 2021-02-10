5f49086d9205ac5c0f39e639577e4ebdb602c8e6
David A. Mentgen III <Flipdavid@gmail.com>
2021-02-10 06:25:38 -0600

    Work In Progress: Initial movement for character relative to camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Character player;
+    public Camera playerCamera;
+    void Start()
+    {
+        player = new Character();
+        if (playerCamera == null)
+        {
+            Debug.LogError("Player Camera Not Set");
+        }
+    }
+
+    void Update()
+    {
+        HandleUserInput();
+    }
+
+    private void HandleUserInput()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) moveDirection += playerCamera.transform.forward;
+        if (Input.GetKey(KeyCode.S)) moveDirection += -playerCamera.transform.forward;
+        if (Input.GetKey(KeyCode.A)) moveDirection += -playerCamera.transform.right;
+        if (Input.GetKey(KeyCode.D)) moveDirection += playerCamera.transform.right;
+
+        moveDirection.y = 0f;
+        transform.position += moveDirection.normalized * 10 * Time.deltaTime;
+
+        if (moveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), 10 * Time.deltaTime);
+        }
+
+        Debug.Log(moveDirection);
+    }
+}
